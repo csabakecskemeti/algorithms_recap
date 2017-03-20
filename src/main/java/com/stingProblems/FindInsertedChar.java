7fa1145991d5a023519599a50f3d1af7a548b96b
kecso <kecso@yahoo-inc.com>
2017-03-20 05:19:40 -0700

    findchar puzzle - unsolved

---
+++ src/main/java/com/stingProblems/FindInsertedChar.java
@@ -0,0 +1,14 @@
+package com.stingProblems;
+
+/**
+ * Created by kecso on 3/14/17.
+ */
+public class FindInsertedChar {
+    String a = "acbde";
+    String b = "abcxed";
+    // res: x
+
+    // naive: hashmap
+
+    // ultimate: xor
+}
